bde323742d2a69c1c890e51923a5980440933a78
brnkath <brnkath@gmail.com>
2023-10-11 12:53:27 -0500

    Update script before final push

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# leaflet-challenge
+# Using Leaflet to map earthquake locarions from USGS data
+
+<img src="images/earthquake-map.png" width="400" align="right" />
+
+<hr>
 
 color pallette from https://www.color-hex.com/color-palette/56220
--- static/js/logic.js
+++ static/js/logic.js
@@ -11,6 +11,25 @@ function createMap(earthquakeLayer) {
     zoom: 4,
     layers: [map, earthquakeLayer],
   });
+
+  // Create the legend
+  let legend = L.control({ position: "bottomright" });
+  legend.onAdd = function () {
+    let div = L.DomUtil.create("div", "legend");
+    let legendInfo =
+      "<div><table><tr><th>Earthquake Depth</th></tr>" +
+      "<tr style='background-color: #44ce1b; font-weight: bold; text-align: center'><td>-10-10</td></tr>" +
+      "<tr style='background-color: #bbdb44; font-weight: bold; text-align: center'><td>10-30</td></tr>" +
+      "<tr style='background-color: #f7e379; font-weight: bold; text-align: center'><td>30-50</td></tr>" +
+      "<tr style='background-color: #f2a134; font-weight: bold; text-align: center'><td>50-70</td></tr>" +
+      "<tr style='background-color: #e51f1f; font-weight: bold; text-align: center'><td>70-90</td></tr>" +
+      "<tr style='background-color: #660000; font-weight: bold; text-align: center'><td>90+</td></tr></table></div>";
+
+    div.innerHTML = legendInfo;
+    return div;
+  };
+
+  legend.addTo(myMap);
 }
 
 // Set url for earthquake data - All earthquakes within the last day
@@ -30,6 +49,7 @@ function createFeatures(earthquakes) {
   // Set up a loop to add all marker information to the earthquake array
   for (let i = 0; i < earthquakes.length; i++) {
     // Set up variables
+    let eventDate = earthquakes[i].properties.time;
     let magnitude = earthquakes[i].properties.mag;
     let longitude = earthquakes[i].geometry.coordinates[0];
     let latitude = earthquakes[i].geometry.coordinates[1];
@@ -62,6 +82,8 @@ function createFeatures(earthquakes) {
         radius: magnitude * 50000,
       }).bindPopup(
         `<h3>Location: ${earthquakes[i].properties.title}</h3><hr>
+        <h4>Time: ${new Date(eventDate)}</h4>
+        <h4>Magnitude: ${magnitude}</h4>
         <h4>Latitude: ${latitude}&#176;</h4>
         <h4>Longitude: ${longitude}&#176;</h4>
         <h4>Depth: ${depth} Kilometers</h4>`
